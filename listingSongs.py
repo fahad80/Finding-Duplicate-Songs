e9366145ef4ca0303affec69b26e739bfaada116
DESKTOP-8MQ7EG9\Fahad Mirza <fahad@haxiot.com>
2018-09-27 17:12:48 -0500

    added doc-string and removed unnecessary import;

--- listingSongs.py
+++ listingSongs.py
@@ -1,6 +1,5 @@
+"""List all the files in a folder"""
 import os
-import sys
-
 
 num_of_song = 0
 song_file = open('listOfSongs.txt', 'w')
@@ -14,7 +13,7 @@ for all_song in os.walk('C:\\Users\\Fahad\\Music'):
         # Just wanted to see what other file format I had
         # split = song.split('.')
         # extension = split[len(split) - 1]
-        
+
         # if (extension != 'mp3' and extension != 'MP3'):
         #     count2 = count2 + 1
         #     print(song)
