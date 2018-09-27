b47c40617dc8427f53e73219f0ae8bc2aaadf1d5
DESKTOP-8MQ7EG9\Fahad Mirza <fahad@haxiot.com>
2018-09-27 16:45:29 -0500

    Following PEP8 convention;

--- FindingDuplicates.py
+++ FindingDuplicates.py
@@ -1,10 +1,10 @@
 import os
 import sys
 
-listOfSongsName = []
-listOfSongsFile = open('listOfSongs.txt','r')
+list_of_songs = []
+list_of_songs_file = open('listOfSongs.txt', 'r')
 
-allSong_str = listOfSongsName.read().lower()
+allSong_str = list_of_songs.read().lower()
 
 
 allSong_list = allSong_str.split('\n\n')
@@ -15,19 +15,19 @@ for song in allSong_list:
     extension = split_words[len(split_words) - 1]
     # now let's separate the extension from the song name
     song_name = song.split('.' + extension)[0]
-    listOfSongsName.append(song_name)
+    list_of_songs.append(song_name)
 
 
 numOfDuplicateSong = 0
 startingIndex = 1
 for songName in listOfSongName:
-    for i in range(startingIndex, len(listOfSongsName)):
-        if songName == listOfSongsName[i]:
+    for i in range(startingIndex, len(list_of_songs)):
+        if songName == list_of_songs[i]:
             print(songName)
             numOfDuplicateSong = numOfDuplicateSong + 1
     # You don't want to start searching from the beginning.
     # Otherwise you will end up comparing with itself
     startingIndex = startingIndex + 1
 
-print('Total Duplicate = ' + str(numOfDuplicateSong) )
-listOfSongsFile.close()
+print('Total Duplicate = ' + str(numOfDuplicateSong))
+list_of_songs_file.close()
