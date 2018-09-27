1811c4b945f3bc156b9d1d1d81a7ef4e048273f4
DESKTOP-8MQ7EG9\Fahad Mirza <fahad@haxiot.com>
2018-09-27 17:10:40 -0500

    Applying PEP8 convention;

--- listingSongs.py
+++ listingSongs.py
@@ -1,26 +1,27 @@
-import os, sys
-numOfSong = 0
-count2 = 0
-listOfSongsFile = open('listOfSongs.txt','w')
+import os
+import sys
 
 
-for allSong in os.walk('C:\\Users\\Fahad\\Music'):
+num_of_song = 0
+song_file = open('listOfSongs.txt', 'w')
+
+
+for all_song in os.walk('C:\\Users\\Fahad\\Music'):
     # allSong is a list of 3-tuple
     # Actual file names are in the 3rd tuple
-    for song in allSong[2]:
+    for song in all_song[2]:
 
         # Just wanted to see what other file format I had
-        #split = song.split('.')
-        #extension = split[len(split) - 1]
+        # split = song.split('.')
+        # extension = split[len(split) - 1]
         
-        #if (extension != 'mp3' and extension != 'MP3'):
-        #    count2 = count2 + 1
-        #    print(song)
+        # if (extension != 'mp3' and extension != 'MP3'):
+        #     count2 = count2 + 1
+        #     print(song)
 
-        listOfSongsFile.write(song)
-        listOfSongsFile.write('\r\n')
-        numOfSong = numOfSong + 1
+        song_file.write(song)
+        song_file.write('\r\n')
+        num_of_song = num_of_song + 1
 
-print('Total files:' + str(numOfSong))
-#print('Total not mp3:' + str(count2))
-listOfSongsFile.close()
+print('Total files:' + str(num_of_song))
+song_file.close()
