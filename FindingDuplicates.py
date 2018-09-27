116857056daa9b8dea4c014910615b23c27a7f3b
DESKTOP-8MQ7EG9\Fahad Mirza <fahad@haxiot.com>
2018-09-27 16:55:01 -0500

    following PEP8 convention;

--- FindingDuplicates.py
+++ FindingDuplicates.py
@@ -13,16 +13,15 @@ for song in allSong_list:
     list_of_songs.append(song_name)
 
 
-numOfDuplicateSong = 0
+num_of_duplicate_songs = 0
 startingIndex = 1
-for songName in listOfSongName:
+for song in list_of_songs:
     for i in range(startingIndex, len(list_of_songs)):
-        if songName == list_of_songs[i]:
-            print(songName)
-            numOfDuplicateSong = numOfDuplicateSong + 1
+        if song == list_of_songs[i]:
+            print(song)
+            num_of_duplicate_songs = num_of_duplicate_songs + 1
     # You don't want to start searching from the beginning.
     # Otherwise you will end up comparing with itself
     startingIndex = startingIndex + 1
 
-print('Total Duplicate = ' + str(numOfDuplicateSong))
-list_of_songs_file.close()
+print('Total Duplicate = ' + str(num_of_duplicate_songs))
