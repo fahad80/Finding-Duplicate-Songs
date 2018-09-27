4d90af586ae00e14e593189f4b43e73918841d14
DESKTOP-8MQ7EG9\Fahad Mirza <fahad@haxiot.com>
2018-09-27 17:07:52 -0500

    Applying PEP8 convention;

--- FindingDuplicates.py
+++ FindingDuplicates.py
@@ -8,22 +8,22 @@ with open('listOfSongs.txt', 'r') as song_file:
 
 for song in allSong_list:
     split_words = song.split('.')
-    # There can be more '.' in the song name. We are interested about the last one
+    # There can be more '.' in the song title. We are interested about the last one
     extension = split_words[len(split_words) - 1]
-    # now let's separate the extension from the song name
-    song_name = song.split('.' + extension)[0]
-    list_of_songs.append(song_name)
+    # now let's separate the extension from the song title
+    song_title = song.split('.' + extension)[0]
+    list_of_songs.append(song_title)
 
 
 num_of_duplicate_songs = 0
-startingIndex = 1
+starting_index = 1
 for song in list_of_songs:
-    for i in range(startingIndex, len(list_of_songs)):
+    for i in range(starting_index, len(list_of_songs)):
         if song == list_of_songs[i]:
             print(song)
             num_of_duplicate_songs = num_of_duplicate_songs + 1
     # You don't want to start searching from the beginning.
     # Otherwise you will end up comparing with itself
-    startingIndex = startingIndex + 1
+    starting_index = starting_index + 1
 
 print('Total Duplicate = ' + str(num_of_duplicate_songs))
